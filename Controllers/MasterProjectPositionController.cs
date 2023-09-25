7c09f93c02d0ba16c16052c9a8a57d4e24c7edd2
erizalmh <eteksimpang@gmail.com>
2023-09-25 09:40:24 +0700

    Commit Tabel Master_Project_Position

---
+++ Controllers/MasterProjectPositionController.cs
@@ -0,0 +1,78 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using TrainingAPI.App_Code;
+
+namespace TrainingAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DStudentController : iFinancingController
+    {
+        public DStudentController(IConfiguration configuration, IWebHostEnvironment hostingEnvironment) : base(configuration, hostingEnvironment)
+        {
+
+        }
+
+        private string _tableName = "MASTER_PROJECT_POSITION";
+        private string _spName = "";
+
+        [HttpPost]
+        [Route("GetRows")]
+        public async Task<IActionResult> GetRows([FromBody] dynamic data)
+        {
+            return await Task.Run(() =>
+            {
+                var Jsondata = JsonConvert.SerializeObject(data);
+                JObject JsonObject = JObject.Parse(Jsondata);
+
+                return Ok(base.GetRows(JsonObject, _tableName));
+            });
+        }
+
+        [HttpPost]
+        [Route("GetRow")]
+        public async Task<IActionResult> GetRow([FromBody] dynamic data)
+        {
+            return await Task.Run(() =>
+            {
+                return Ok(base.GetRow((JArray)data, _tableName));
+            });
+        }
+
+        [HttpPost]
+        [Route("Insert")]
+        public async Task<IActionResult> Insert([FromBody] dynamic data)
+        {
+            return await Task.Run(() =>
+            {
+                return Ok(base.Insert((JArray)data, _tableName));
+            });
+        }
+
+        [HttpPost]
+        [Route("Update")]
+        public async Task<IActionResult> Update([FromBody] dynamic data)
+        {
+            return await Task.Run(() =>
+            {
+                return Ok(base.Update((JArray)data, _tableName));
+            });
+        }
+
+        [HttpPost]
+        [Route("Delete")]
+        public async Task<IActionResult> Delete([FromBody] dynamic data)
+        {
+            return await Task.Run(() =>
+            {
+                return Ok(base.Delete((JArray)data, _tableName));
+            });
+        }
+    }
+}
